9dc42fbf992f1001afda96cbc21b74f91c0501ea
Joao Vitor Caversan <joaovitorcaversan@gmail.com>
2024-03-11 19:30:28 -0300

    feat: model tasks

---
+++ src/models/task-model.ts
@@ -0,0 +1,36 @@
+import { timeStamp } from "console";
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    categoryId: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
